8262e020315e9324df4b4a3508192892fcf2fb1e
Kabir <kabir4691@gmail.com>
2020-07-24 18:30:21 +0530

    Show discounted price and savings instead of total price

--- cart.rb
+++ cart.rb
@@ -8,7 +8,29 @@ class Cart
     @items << grocery_item
   end
 
-  def bill_amount 
-    @items.map { |item| item.price }.inject(:+)
+  def bill_amount
+    return [0, 0] if @items.empty? 
+
+    total_amount = @items.map { |item| item.price }.inject(:+)
+    items_group = @items.group_by { |item| item.name }
+    discounted_amount = items_group.inject(0) { |total, (name, list)| 
+      total += collection_bulk_price(list)
+    }   
+
+    [total_amount, discounted_amount]
+  end
+  
+  private
+
+  def collection_bulk_price(collection)
+    return 0 if (collection.nil? || collection.empty?)
+
+    size = collection.size
+    item = collection[0]
+    return size * item.price if item.sale_amount.nil?
+    
+    quotient = size / item.sale_amount
+    remainder = size % item.sale_amount
+    (quotient * item.sale_price) + (remainder * item.price)
   end
 end
